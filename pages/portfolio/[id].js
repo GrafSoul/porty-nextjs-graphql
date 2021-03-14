634becb520822b7d81aafd48dcd072a337ba2005
GrafSoul <grafsoul@gmail.com>
2021-03-14 12:43:50 +0500

    feat: Changes data loading on the Portfolio Page.

--- pages/portfolio/[id].js
+++ pages/portfolio/[id].js
@@ -1,61 +1,68 @@
+// React
+import React, { useState, useEffect } from 'react';
 // Apollo
-import { useQuery } from '@apollo/client';
+import { useLazyQuery } from '@apollo/client';
 import { GET_PORTFOLIO } from '@/apollo/queries';
 
 const PortfolioDetails = ({ query }) => {
-    const { loading, error, data } = useQuery(GET_PORTFOLIO, {
-        variables: { id: query.id },
-    });
-    const portfolio = (data && data.portfolio) || {};
+    const [portfolio, setPortfolio] = useState(null);
 
-    if (loading) return 'Loading...';
+    useEffect(() => {
+        getPortfolio({
+            variables: { id: query.id },
+        });
+    }, []);
+
+    const [getPortfolio, { loading, error, data }] = useLazyQuery(
+        GET_PORTFOLIO,
+    );
+    if (data && !portfolio) setPortfolio(data.portfolio);
+    if (loading || !portfolio) return 'Loading...';
     if (error) return `Error! ${error.message}`;
 
     return (
         <>
-            {portfolio && (
-                <div className="portfolio-detail">
-                    <div className="container">
-                        <div className="jumbotron">
-                            <h1 className="display-3">{portfolio.title}</h1>
-                            <p className="lead">{portfolio.jobTitle}</p>
-                            <p>
-                                <a
-                                    className="btn btn-lg btn-success"
-                                    href={portfolio.companyWebsite}
-                                    role="button"
-                                >
-                                    See Company
-                                </a>
-                            </p>
+            <div className="portfolio-detail">
+                <div className="container">
+                    <div className="jumbotron">
+                        <h1 className="display-3">{portfolio.title}</h1>
+                        <p className="lead">{portfolio.jobTitle}</p>
+                        <p>
+                            <a
+                                className="btn btn-lg btn-success"
+                                href={portfolio.companyWebsite}
+                                role="button"
+                            >
+                                See Company
+                            </a>
+                        </p>
+                    </div>
+
+                    <div className="row marketing">
+                        <div className="col-lg-6">
+                            <h4 className="title">Location</h4>
+                            <p className="text">{portfolio.location}</p>
+
+                            <h4 className="title">Start Date</h4>
+                            <p className="text">{portfolio.startDate}</p>
                         </div>
 
-                        <div className="row marketing">
-                            <div className="col-lg-6">
-                                <h4 className="title">Location</h4>
-                                <p className="text">{portfolio.location}</p>
+                        <div className="col-lg-6">
+                            {/* TODO: days later... */}
+                            <h4 className="title">Days</h4>
+                            <p className="text">44</p>
 
-                                <h4 className="title">Start Date</h4>
-                                <p className="text">{portfolio.startDate}</p>
-                            </div>
-
-                            <div className="col-lg-6">
-                                {/* TODO: days later... */}
-                                <h4 className="title">Days</h4>
-                                <p className="text">44</p>
-
-                                <h4 className="title">End Date</h4>
-                                <p className="text">{portfolio.endDate}</p>
-                            </div>
-                            <div className="col-md-12">
-                                <hr />
-                                <h4 className="title">Description</h4>
-                                <p>{portfolio.description}</p>
-                            </div>
+                            <h4 className="title">End Date</h4>
+                            <p className="text">{portfolio.endDate}</p>
+                        </div>
+                        <div className="col-md-12">
+                            <hr />
+                            <h4 className="title">Description</h4>
+                            <p>{portfolio.description}</p>
                         </div>
                     </div>
                 </div>
-            )}
+            </div>
         </>
     );
 };
