17778ef4c09dc5aa83203fcbfa7c94c53cce0745
GrafSoul <grafsoul@gmail.com>
2021-03-22 13:17:14 +0500

    feat: Added RegisterForm component.

--- pages/register.js
+++ pages/register.js
@@ -1,5 +1,22 @@
+import RegisterForm from '@/components/RegisterForm';
+
 const Register = () => {
-    return <h1>Register</h1>;
+    const register = (registerData) => {
+        alert(JSON.stringify(registerData));
+    };
+
+    return (
+        <>
+            <div className="bwm-form mt-5">
+                <div className="row">
+                    <div className="col-md-5 mx-auto">
+                        <h1 className="page-title">Register</h1>
+                        <RegisterForm onSubmit={register} />
+                    </div>
+                </div>
+            </div>
+        </>
+    );
 };
 
 export default Register;
