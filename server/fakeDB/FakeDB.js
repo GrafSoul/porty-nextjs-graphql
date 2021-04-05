8e74b0fe08ac9fa127fb52651b0bab4d99811046
GrafSoul <grafsoul@gmail.com>
2021-04-05 14:48:33 +0500

    feat: added Topics to FakeDB.

--- server/fakeDB/FakeDB.js
+++ server/fakeDB/FakeDB.js
@@ -1,21 +1,24 @@
 // Seed Data
-const { portfolios, users, forumCategories } = require('./data');
+const { portfolios, users, forumCategories, topics } = require('./data');
 // Models
 const Portfolio = require('../database/models/portfolio');
 const User = require('../database/models/user');
 const ForumCategory = require('../database/models/forumCategory');
+const Topic = require('../database/models/topic');
 
 class FakeDB {
     async cleanData() {
         await User.deleteMany({});
         await Portfolio.deleteMany({});
         await ForumCategory.deleteMany({});
+        await Topic.deleteMany({});
     }
 
     async addData() {
         await User.create(users);
         await Portfolio.create(portfolios);
         await ForumCategory.create(forumCategories);
+        await Topic.create(topics);
     }
 
     async populate() {
