88ba2e7aa8f302862b9d077917d8dbebbe316838
GrafSoul <grafsoul@gmail.com>
2021-04-14 17:02:50 +0500

    feat: Created display posts.

--- server/fakeDB/data.js
+++ server/fakeDB/data.js
@@ -30,7 +30,7 @@ const data = {
         {
             _id: user1Id,
             avatar:
-                'https://f0.pngfuel.com/png/312/283/man-face-clip-art-png-clip-art-thumbnail.png',
+                'https://cdn1.iconfinder.com/data/icons/ninja-things-1/1772/ninja-simple-512.png',
             email: 'admin@mail.com',
             name: 'Admin Developer',
             username: 'Administrator',
@@ -41,7 +41,7 @@ const data = {
         {
             _id: user2Id,
             avatar:
-                'https://f0.pngfuel.com/png/312/283/man-face-clip-art-png-clip-art-thumbnail.png',
+                'https://cdn.icon-icons.com/icons2/1736/PNG/512/4043260-avatar-male-man-portrait_113269.png',
             email: 'instructor@mail.com',
             name: 'Instructor Developer',
             username: 'Instructor',
